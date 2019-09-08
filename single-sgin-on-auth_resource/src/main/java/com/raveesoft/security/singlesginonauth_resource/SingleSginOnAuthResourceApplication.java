238e3987f204a17adc07f476b2ecc15739ce9666
Malliyawadu Raveera Veranga De Silva  (NCS) <rv.malliyawadu@ncs.com.sg>
2019-09-08 20:00:45 +0800

    add auth resource project

---
+++ single-sgin-on-auth_resource/src/main/java/com/raveesoft/security/singlesginonauth_resource/SingleSginOnAuthResourceApplication.java
@@ -0,0 +1,13 @@
+package com.raveesoft.security.singlesginonauth_resource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SingleSginOnAuthResourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SingleSginOnAuthResourceApplication.class, args);
+	}
+
+}
