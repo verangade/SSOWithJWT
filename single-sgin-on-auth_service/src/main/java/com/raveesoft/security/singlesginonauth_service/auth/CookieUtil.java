f73585cd2a50d4a87b6303f115b150ebf94a0913
P0102475 <P0102475@NCS-021018JJ09.ncs.corp.int-ads>
2019-09-08 22:43:12 +0800

    Add Cookie util
    cookie.setSecure(secure): secure=true => work on HTTPS only.
    
    cookie.setHttpOnly(true): invisible to JavaScript.
    
    cookie.setMaxAge(maxAge): maxAge=0: expire cookie now, maxAge<0: expire
    cookiie on browser exit.
    
    cookie.setDomain(domain): visible to domain only.
    
    cookie.setPath("/"): visible to all paths.

---
+++ single-sgin-on-auth_service/src/main/java/com/raveesoft/security/singlesginonauth_service/auth/CookieUtil.java
@@ -0,0 +1,49 @@
+package com.raveesoft.security.singlesginonauth_service.auth;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.util.WebUtils;
+
+
+/**
+ * 
+ * 
+cookie.setSecure(secure): secure=true => work on HTTPS only.
+
+cookie.setHttpOnly(true): invisible to JavaScript.
+
+cookie.setMaxAge(maxAge): maxAge=0: expire cookie now, maxAge<0: expire cookiie on browser exit.
+
+cookie.setDomain(domain): visible to domain only.
+
+cookie.setPath("/"): visible to all paths.
+ *
+ */
+
+public class CookieUtil {
+	public static void create(HttpServletResponse httpServletResponse,String name , String value, Boolean secure , Integer maxAge , String domain) {
+		Cookie cookie = new Cookie(name, value);
+		cookie.setSecure(secure);
+		cookie.setHttpOnly(true);
+		cookie.setMaxAge(maxAge);
+		cookie.setDomain(domain);
+		cookie.setPath("/");
+		httpServletResponse.addCookie(cookie);
+	}
+	
+	public static void clear(HttpServletResponse httpServletResponse,String name) {
+		Cookie cookie = new Cookie(	name, null);
+		cookie.setPath("/");
+		cookie.setHttpOnly(true);
+		cookie.setMaxAge(0);
+		httpServletResponse.addCookie(cookie);
+	}
+	
+	public static String getValue(HttpServletRequest httpServletRequest,String name) {
+		Cookie cookie = WebUtils.getCookie(httpServletRequest, name);
+		return cookie != null ? cookie.getValue() : null;
+	}
+	
+}
