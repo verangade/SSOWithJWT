b2d19b3fdc27cffe1f65d61b6860a1aba1777f5f
Malliyawadu Raveera Veranga De Silva  (NCS) <rv.malliyawadu@ncs.com.sg>
2019-09-08 19:55:39 +0800

    Intilize auth service

---
+++ single-sgin-on-auth_service/src/test/java/com/raveesoft/security/singlesginonauth_service/SingleSginOnAuthServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.raveesoft.security.singlesginonauth_service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SingleSginOnAuthServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
